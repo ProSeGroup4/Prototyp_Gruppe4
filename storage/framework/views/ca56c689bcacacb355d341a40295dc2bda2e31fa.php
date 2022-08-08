3812bf93c4324192cabd72c7aba8065b204ddcc6
ProSeGroup4 <107424490+ProSeGroup4@users.noreply.github.com>
2022-08-08 13:19:53 +0200

    Delete storage directory

--- storage/framework/views/ca56c689bcacacb355d341a40295dc2bda2e31fa.php
+++
@@ -1,4 +0,0 @@
-<?php $__env->startSection("content"); ?>
-    Index
-<?php $__env->stopSection(); ?>
-<?php echo $__env->make("layouts.app", \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH /Applications/XAMPP/xamppfiles/htdocs/Prototyp2/resources/views/posts/index.blade.php ENDPATH**/ ?>
